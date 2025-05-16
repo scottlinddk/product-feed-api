26d206fa1182c770c14030193d798af607a60f1d
Scott Lind <74186414+scottlinddk@users.noreply.github.com>
2025-05-16 19:56:42 +0200

    Update feedRoutes.ts

--- src/routes/feedRoutes.ts
+++ src/routes/feedRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 import { getProducts } from "../controllers/feedController.js";
 
-import path from 'path';
+const path = require('path');
 
 const baseUrl: string = "/feed/";
 
